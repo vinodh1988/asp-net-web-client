7ce8eb4df90d19ed9600543436af47808514828d
vinodh1988 <k.c.vinodh@hotmail.com>
2022-09-13 17:02:17 +0530

    first-commit

---
+++ wwwroot/js/apicall.js
@@ -0,0 +1,17 @@
+﻿console.log("Code is executing")
+
+$.get("http://localhost:5000/api/peopledb", function (data, status) {
+
+    if (data) {
+        let code=""
+        for (let x in data) {
+            code += "<tr>"
+            code += "<td>" + data[x].sno +"</td>"
+            code += "<td>" + data[x].name + "</td>"
+            code += "<td>" + data[x].city + "</td>"
+            code += "</tr>"
+        }
+        $("#tdata").html(code)
+    }
+
+})
