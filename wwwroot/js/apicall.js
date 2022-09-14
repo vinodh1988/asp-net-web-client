9d2268a0341ef04ec15781ca01fdde368609ae4a
vinodh1988 <k.c.vinodh@hotmail.com>
2022-09-14 15:14:25 +0530

    api-call-post

--- wwwroot/js/apicall.js
+++ wwwroot/js/apicall.js
@@ -1,17 +1,54 @@
 ﻿console.log("Code is executing")
 
-$.get("http://localhost:5000/api/peopledb", function (data, status) {
+function dataread (){
+    $.get("http://localhost:5000/api/peopledb", function (data, status) {
 
-    if (data) {
-        let code=""
-        for (let x in data) {
-            code += "<tr>"
-            code += "<td>" + data[x].sno +"</td>"
-            code += "<td>" + data[x].name + "</td>"
-            code += "<td>" + data[x].city + "</td>"
-            code += "</tr>"
+        if (data) {
+            let code = ""
+            for (let x in data) {
+                code += "<tr>"
+                code += "<td>" + data[x].sno + "</td>"
+                code += "<td>" + data[x].name + "</td>"
+                code += "<td>" + data[x].city + "</td>"
+                code += "</tr>"
+            }
+            $("#tdata").html(code)
         }
-        $("#tdata").html(code)
-    }
 
-})
+    })
+}
+
+dataread()
+
+$(document).ready(
+    function () {
+        $('#store').click(function () {
+
+
+            $.ajax({
+                url: 'http://localhost:5000/api/peopledb',
+                dataType: 'json',
+                type: 'post',
+                contentType: 'application/json',
+                data: JSON.stringify({
+                    sno: $('#sno').val(),
+                    name: $('#name').val(),
+                    city: $('#city').val()
+                }),
+                processData: false,
+                success: function (data, textStatus, jQxhr) {
+                    alert("data successfully stored")
+                    dataread()
+
+                },
+                error: function (jqXhr, textStatus, errorThrown) {
+                    alert("problem in storing the data")
+                }
+            });
+
+
+
+        })
+      
+    }
+)
